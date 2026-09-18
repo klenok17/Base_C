dcd33b9dfe85807a42c1804904cdb1154813f1c3
Oksana Klenina <miptwork17@gmail.com>
2026-09-18 16:13:10 +0300

    Work 03

---
+++ HW3/task5.c
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c;
+	double average;
+	
+	scanf("%d %d %d", &a, &b, &c);
+	average = (a + b + c) / 3.0;
+	printf("%.2f\n", average);    
+	      
+           
+	return 0;
+}
+
